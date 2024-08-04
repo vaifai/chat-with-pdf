eae9a15ff690238584f6b01037eb6234884f37e9
vaifai <vaifaipandey1996@gmail.com>
2024-08-04 18:21:02 +0530

    Landing page

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,7 @@ export default function RootLayout({
   return (
       <ClerkProvider>
         <html lang="en">
-          <body>{children}</body>
+          <body className="min-h-screen h-screen overflow-hidden flex flex-col">{children}</body>
         </html>
       </ClerkProvider>
   );
